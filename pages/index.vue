7d1198cce4d8f14fda5aa9beb995adcfea55fa93
ando105 <do.phuan@gmail.com>
2020-09-02 10:09:53 +0700

    Init project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-container">
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.page-container {
+  margin: 0;
+  min-height: 100vh;
+  display: flex;
+}
+</style>
